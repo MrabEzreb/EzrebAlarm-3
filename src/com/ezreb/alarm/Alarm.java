5ca8f6e17fe827dc492a099311e9f4809747577b
MrabEzreb <de013b3f@opayq.com>
2015-03-02 14:39:22 -0500

    :P

--- src/com/ezreb/alarm/Alarm.java
+++ src/com/ezreb/alarm/Alarm.java
@@ -1,9 +1,30 @@
 package com.ezreb.alarm;
 
-public class Alarm {
+import java.io.File;
+import java.io.Serializable;
 
+import com.ezreb.alarm.util.DataOutputStream;
+import com.ezreb.alarm.util.Refrence;
+
+public class Alarm implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5120772799072450134L;
+	public Alarm(String name) {
+		this.name = name;
+	}
 	private String name;
 	public void saveToFile() {
-		
+		File saveTo = new File(Refrence.alarms, name+".alarm");
+		DataOutputStream out = new DataOutputStream(saveTo);
+		out.writeObject(this);
+	}
+	public static void main(String[] args) {
+		Refrence.home.mkdirs();
+		Refrence.alarms.mkdirs();
+		Alarm a = new Alarm("Test");
+		a.saveToFile();
 	}
 }
--- src/com/ezreb/alarm/util/Refrence.java
+++ src/com/ezreb/alarm/util/Refrence.java
@@ -4,5 +4,9 @@ import java.io.File;
 
 public class Refrence {
 
-	public static final File home = new File(System.getProperty("user.home"), "Roaming/Ezreb/EzrebAlarm");
+	
+	public static final File home = new File(System.getProperty("user.home"), "/Roaming/Ezreb/EzrebAlarm");
+	public static final File alarms = new File(home, "Alarms");
+	static {
+	}
 }
